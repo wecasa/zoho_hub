e9d5d3450cd58ca0ca895e1b806362a256732833
Julien Lestavel <julien@wecasa.fr>
2023-01-05 23:39:13 +0100

    Better error handling

--- lib/zoho_hub/base_record.rb
+++ lib/zoho_hub/base_record.rb
@@ -167,32 +167,13 @@ module ZohoHub
 
       alias exist? exists?
 
-      # rubocop:disable Metrics/AbcSize
-      # rubocop:disable Metrics/CyclomaticComplexity
-      # rubocop:disable Metrics/PerceivedComplexity
       def build_response(body)
         response = Response.new(body)
 
-        raise InvalidTokenError, response.msg if response.invalid_token?
-        raise InternalError, response.msg if response.internal_error?
-        raise InvalidRequestError, response.msg if response.invalid_request?
-        raise InvalidQueryError, response.msg if response.invalid_query?
-        raise RecordInvalid, response.msg if response.invalid_data?
-        raise InvalidModule, response.msg if response.invalid_module?
-        raise NoPermission, response.msg if response.no_permission?
-        raise AuthenticationFailure, response.msg if response.authentication_failure?
-        raise MandatoryNotFound, response.msg if response.mandatory_not_found?
-        raise RecordInBlueprint, response.msg if response.record_in_blueprint?
-        raise TooManyRequestsError, response.msg if response.too_many_requests?
-        raise RecordNotInProcessError, response.msg if response.record_not_in_process?
-        raise RecordNotFound, response.msg if response.record_not_found?
-        raise OauthScopeMismatch, response.msg if response.oauth_scope_mismatch?
+        raise response.error_class, response.msg if response.error?
 
         response
       end
-      # rubocop:enable Metrics/AbcSize
-      # rubocop:enable Metrics/CyclomaticComplexity
-      # rubocop:enable Metrics/PerceivedComplexity
     end
 
     def initialize(params = {})
--- spec/zoho_hub/connection_spec.rb
+++ spec/zoho_hub/connection_spec.rb
@@ -57,4 +57,39 @@ RSpec.describe ZohoHub::Connection do
       end
     end
   end
+
+  describe '#with_refresh' do
+    subject(:connection) do
+      described_class.new(
+        access_token: 'TOKEN_1', expires_in: 'EXPIRES_IN_SEC', refresh_token: 'REFRESH_TOKEN',
+        api_domain: ZohoHub.configuration.api_domain
+      )
+    end
+
+    before do
+      allow(ZohoHub).to receive(:configuration).and_return(ZohoHub::Configuration.new)
+    end
+
+    let!(:get_stub) do
+      stub_request(:get, ZohoHub.configuration.api_domain)
+        .to_return([
+                     { status: 400, body: { code: 'INVALID_TOKEN', status: 'error' }.to_json },
+                     { status: 200, body: '' }
+                   ])
+    end
+
+    let!(:token_refresh_stub) do
+      stub_request(:post, "#{connection.api_domain}/oauth/v2/token?client_id=" \
+                          '&client_secret=&grant_type=refresh_token&refresh_token=REFRESH_TOKEN')
+        .and_return(body: { access_token: 'TOKEN_2' }.to_json)
+    end
+
+    it 'refreshes access token if invalid' do
+      expect do
+        connection.get('/')
+      end.to change(connection, :access_token).from('TOKEN_1').to('TOKEN_2')
+      expect(get_stub).to have_been_requested.twice
+      expect(token_refresh_stub).to have_been_requested
+    end
+  end
 end
