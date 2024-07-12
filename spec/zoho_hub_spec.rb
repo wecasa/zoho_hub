dc2487ccd5c198d6fdfe000696ad07c8c74634de
qen <qen.empaces@gmail.com>
2024-07-12 14:36:04 +0800

    zoho_hub spec green

--- spec/zoho_hub_spec.rb
+++ spec/zoho_hub_spec.rb
@@ -4,4 +4,15 @@ RSpec.describe ZohoHub do
   it 'has a version number' do
     expect(ZohoHub::VERSION).not_to be nil
   end
+
+  describe '#on_initialize_connection' do
+    let(:proc) { double('Proc', call: true) }
+
+    it "calls the proc once" do
+      expect do |block|
+        ZohoHub.on_initialize_connection(&block)
+        ZohoHub.connection.send(:adapter)
+      end.to yield_control
+    end
+  end
 end
